6bb5d16a8615b32a090f843716d9650e40c38558
Will Shanks <git@wshanks.com>
2024-06-27 12:25:05 -0700

    Working with hard coded config

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Fake Radius auth
 This is a pam module designed to add a random sleep and acceptance chance to requests to help us debug service auth flows without hitting the production Radius server.
-This might get split into multiple pam modules, ex: pam_random_sleep and pam_radom_deny
+This might get split into multiple pam modules, ex: `pam_random_sleep` and `pam_radom_deny`.
+
+## setup
+1. build the code
+1. put so in `/lib/security/`
+1. update pam config
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,57 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+extern crate pam;
+extern crate rand;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use pam::constants::{PamFlag, PamResultCode};
+use pam::module::{PamHandle, PamHooks};
+use rand::Rng;
+use std::ffi::CStr;
+use std::{thread, time};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+struct PamFake;
+pam::pam_hooks!(PamFake);
+
+impl PamHooks for PamFake {
+    // This function performs the task of authenticating the user.
+    fn sm_authenticate(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
+        let max_sleep = 60;
+        let min_sleep = 10;
+        let success_chance = 0.5;
+
+        /*
+        //TODO: collect args from pam config
+        //min_sleep (u32) seconds
+        //max_sleep (u32) seconds
+        //success_chance (float) decimal
+        let args: HashMap<&str, &str> = args
+            .iter()
+            .map(|s| {
+                let mut parts = s.splitn(2, "=");
+                (parts.next().unwrap(), parts.next().unwrap_or(""))
+            })
+            .collect();
+        */
+
+        let mut rng = rand::thread_rng();
+        let sleep_sec = min_sleep + (rng.gen::<u64>() % (max_sleep - min_sleep));
+        let pass: bool = rng.gen::<f64>() < success_chance;
+
+        eprintln!("[DEBUG]: sleep: {} pass: {}", sleep_sec, pass);
+
+        thread::sleep(time::Duration::from_secs(sleep_sec));
+        if pass {
+            PamResultCode::PAM_SUCCESS
+        } else {
+            PamResultCode::PAM_AUTH_ERR
+        }
+    }
+
+    fn sm_setcred(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
+        println!("set credentials");
+        PamResultCode::PAM_SUCCESS
+    }
+
+    fn acct_mgmt(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
+        println!("account management");
+        PamResultCode::PAM_SUCCESS
     }
 }
