1acbcff16748925b17da2b190d8c38a5dc9d1631
Will Shanks <git@wshanks.com>
2024-06-27 18:27:23 -0700

    make sleep range, success rate config args

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,15 @@ use pam::module::{PamHandle, PamHooks};
 use rand::Rng;
 use std::ffi::CStr;
 use std::{thread, time};
+use std::collections::HashMap;
 
 struct PamFake;
 pam::pam_hooks!(PamFake);
 
 impl PamHooks for PamFake {
     // This function performs the task of authenticating the user.
-    fn sm_authenticate(_pamh: &mut PamHandle, _args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
-        let max_sleep = 60;
-        let min_sleep = 10;
-        let success_chance = 0.5;
+    fn sm_authenticate(_pamh: &mut PamHandle, args: Vec<&CStr>, _flags: PamFlag) -> PamResultCode {
 
-        /*
         //TODO: collect args from pam config
         //min_sleep (u32) seconds
         //max_sleep (u32) seconds
@@ -25,11 +22,18 @@ impl PamHooks for PamFake {
         let args: HashMap<&str, &str> = args
             .iter()
             .map(|s| {
-                let mut parts = s.splitn(2, "=");
+                
+                let mut parts = s.to_str().unwrap().splitn(2, "=");
                 (parts.next().unwrap(), parts.next().unwrap_or(""))
             })
             .collect();
-        */
+
+        let zero = "0";
+        let one = "1";
+        eprintln!("[DEBUG]: args: {:?}", args);
+        let min_sleep = args.get("min_sleep").unwrap_or(&zero).parse().unwrap_or(0);
+        let max_sleep = args.get("max_sleep").unwrap_or(&&min_sleep.to_string().as_str()).parse().unwrap_or(min_sleep);
+        let success_chance: f64 = args.get("success_chance").unwrap_or(&one).parse().unwrap_or(1.);
 
         let mut rng = rand::thread_rng();
         let sleep_sec = min_sleep + (rng.gen::<u64>() % (max_sleep - min_sleep));
